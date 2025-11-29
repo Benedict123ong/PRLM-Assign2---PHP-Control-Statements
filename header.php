176cff179b1fba0713a750a39834460eb1de25b6
Benedict Ong <bong25524@gmail.com>
2025-11-29 06:37:36 -1100

    Update header to reflect new store name and structure

--- header.php
+++ header.php
@@ -1,2 +1,15 @@
-<h1>Ratatat Gun Store</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ratatat Armory</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
 
+<header class="main-header">
+    <h1>Ratatat Armory</h1>
+    <nav class="nav">
+        <a href="index.php">Home</a>
+        <a href="products.php">Firearms</a>
+    </nav>
+</header>
